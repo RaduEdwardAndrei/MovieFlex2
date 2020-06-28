5a2a143b2db92fff57c2ad97333d31935db3d35a
Radu Edward-Andrei <raduedwardandrei97@yahoo.com>
2020-06-29 00:54:22 +0300

    delete backend

--- backend/data/index.js
+++
@@ -1,7 +0,0 @@
-const Users = require('./models/user.models');
-const Tokens = require('./models/token.models');
-
-module.exports = {
-    Users,
-    Tokens
-}
--- backend/routes/index.js
+++
@@ -1,7 +0,0 @@
-const Router = require('express')();
-
-const UsersController = require('../Users/controller.js');
-
-Router.use('/users', UsersController);
-
-module.exports = Router;
