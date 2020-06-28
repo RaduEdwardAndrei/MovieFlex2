f2c97689969b2e1018ca45524c74f534fa47bda4
Radu Edward-Andrei <raduedwardandrei97@yahoo.com>
2020-06-29 00:59:45 +0300

    Updates

---
+++ precache-manifest.277f87a060459a868e809f101bd0115b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31306c6ffd67889319ed2209452902d6",
+    "url": "/MovieFlex2/index.html"
+  },
+  {
+    "revision": "26fa23eb00b67a70918e",
+    "url": "/MovieFlex2/static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "0d73d431247646c67097",
+    "url": "/MovieFlex2/static/css/main.cc9ce9aa.chunk.css"
+  },
+  {
+    "revision": "26fa23eb00b67a70918e",
+    "url": "/MovieFlex2/static/js/2.e86de260.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/MovieFlex2/static/js/2.e86de260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d73d431247646c67097",
+    "url": "/MovieFlex2/static/js/main.904321db.chunk.js"
+  },
+  {
+    "revision": "391a86ac7163bfeebe03",
+    "url": "/MovieFlex2/static/js/runtime-main.cd53c83b.js"
+  },
+  {
+    "revision": "efe4fbe85e64f615e00ff789de334780",
+    "url": "/MovieFlex2/static/media/backgroundpic.efe4fbe8.png"
+  }
+]);
